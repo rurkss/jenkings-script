486991574cc7a5a7b65e493bb190f089a1facaf2
Artur Zheludkov <rurkss@gmail.com>
2024-06-12 16:47:03 -0400

    Update app.groovy

--- vars/app.groovy
+++ vars/app.groovy
@@ -16,30 +16,29 @@ def call() {
                     sh 'echo "Running in the busybox-agent node"'
                 }
             }
-            stage('Nested Pod Stage') {
-                podTemplate(
-                    label: 'nested-agent',
-                    containers: [
-                        containerTemplate(
-                            name: 'maven',
-                            image: 'maven:3.8.4-jdk-11',
-                            command: 'cat',
-                            ttyEnabled: true
-                        )
-                    ]
-                ) {
-                    node('nested-agent') {
-                        stage('Run Maven Version in Nested Pod') {
-                            container('maven') {
-                                sh 'mvn --version'
-                            }
-                        }
-                        stage('Create Hello World File in Nested Pod') {
-                            container('maven') {
-                                sh 'echo "Hello, World!" > /tmp/hello.txt'
-                            }
-                        }
-                    }
+        }
+    }
+
+    podTemplate(
+        label: 'nested-agent',
+        containers: [
+            containerTemplate(
+                name: 'maven',
+                image: 'maven:3.8.4-jdk-11',
+                command: 'cat',
+                ttyEnabled: true
+            )
+        ]
+    ) {
+        node('nested-agent') {
+            stage('Run Maven Version in Nested Pod') {
+                container('maven') {
+                    sh 'mvn --version'
+                }
+            }
+            stage('Create Hello World File in Nested Pod') {
+                container('maven') {
+                    sh 'echo "Hello, World!" > /tmp/hello.txt'
                 }
             }
         }

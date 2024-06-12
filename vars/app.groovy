36ae4d6335e4d51db581cad11d2f0b24fb88687c
Artur Zheludkov <rurkss@gmail.com>
2024-06-12 16:09:46 -0400

    Update app.groovy

--- vars/app.groovy
+++ vars/app.groovy
@@ -2,14 +2,41 @@
 
 def call() {
     pipeline {
-        agent any
-
-        stages {
-            stage('Hello World') {
-                steps {
-                    echo 'Hello World'
-                }
+        agent {
+            kubernetes {
+            yaml '''
+                apiVersion: v1
+                kind: Pod
+                spec:
+                containers:
+                - name: maven
+                    image: maven:alpine
+                    command:
+                    - cat
+                    tty: true
+                - name: node
+                    image: node:16-alpine3.12
+                    command:
+                    - cat
+                    tty: true
+                '''
             }
         }
-    }
+        stages {
+            stage('Run maven') {
+            steps {
+                container('maven') {
+                sh 'mvn -version'
+                sh ' echo Hello World > hello.txt'
+                sh 'ls -last'
+                }
+                container('node') {
+                sh 'npm version'
+                sh 'cat hello.txt'
+                sh 'ls -last'
+                }
+            }
+            }
+        }
+}
 }

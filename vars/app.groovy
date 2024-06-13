0c19dcb231dd8f50ce32704b92e35b0f81d5ccf1
Artur Zheludkov <rurkss@gmail.com>
2024-06-13 10:47:21 -0400

    Update app.groovy

--- vars/app.groovy
+++ vars/app.groovy
@@ -26,37 +26,35 @@ def call() {
             stage('Create and Stash File in busybox-agent') {
                 container('busybox') {
                     sh '''
-                        echo "This is a test file from busybox-agent" > /tmp/testfile.txt
-                        echo "busybox-agent Pod IP: ${POD_IP}" >> /tmp/testfile.txt
-                        tar -cvf /tmp/testfile.tar -C /tmp testfile.txt
+                        echo "This is a test file from busybox-agent" > testfile.txt
+                        echo "busybox-agent Pod IP: ${POD_IP}" >> testfile.txt
+                        tar -cvf testfile.tar testfile.txt
                     '''
-                    stash includes: '/tmp/testfile.tar', name: 'testfile-tar'
+                    stash includes: 'testfile.tar', name: 'testfile-tar'
                 }
             }
+        }
+    }
 
-            stage('Nested Pod Stage') {
-                podTemplate(
-                    label: 'nested-agent',
-                    containers: [
-                        containerTemplate(
-                            name: 'busybox',
-                            image: 'busybox',
-                            command: 'cat',
-                            ttyEnabled: true
-                        )
-                    ]
-                ) {
-                    node('nested-agent') {
-                        container('busybox') {
-                            stage('Unstash and Read File in Nested Pod') {
-                                unstash 'testfile-tar'
-                                sh '''
-                                    tar -xvf /tmp/testfile.tar -C /tmp
-                                    cat /tmp/testfile.txt
-                                '''
-                            }
-                        }
-                    }
+    podTemplate(
+        label: 'nested-agent',
+        containers: [
+            containerTemplate(
+                name: 'busybox',
+                image: 'busybox',
+                command: 'cat',
+                ttyEnabled: true
+            )
+        ]
+    ) {
+        node('nested-agent') {
+            stage('Unstash and Read File in Nested Pod') {
+                container('busybox') {
+                    unstash 'testfile-tar'
+                    sh '''
+                        tar -xvf testfile.tar
+                        cat testfile.txt
+                    '''
                 }
             }
         }

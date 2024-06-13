66ecf8ad7eb27ac8640ed0b026f5e38103b8be57
Artur Zheludkov <rurkss@gmail.com>
2024-06-13 10:26:16 -0400

    Update app.groovy

--- vars/app.groovy
+++ vars/app.groovy
@@ -1,5 +1,4 @@
 def call() {
-    // Define the first podTemplate for busybox-agent
     podTemplate(
         label: 'busybox-agent',
         containers: [
@@ -14,13 +13,13 @@ def call() {
         node('busybox-agent') {
             stage('Initial Stage') {
                 container('busybox') {
-                    sh '''
-                        echo "Running in the busybox-agent node"
-                        echo "Getting IP address of busybox-agent pod"
-                        POD_IP=$(hostname -i)
-                        echo "busybox-agent Pod IP: $POD_IP"
-                        echo $POD_IP > /tmp/busybox-agent-ip.txt
-                    '''
+                    script {
+                        env.POD_IP = sh(
+                            script: 'hostname -i',
+                            returnStdout: true
+                        ).trim()
+                        echo "busybox-agent Pod IP: ${env.POD_IP}"
+                    }
                 }
             }
 
@@ -39,11 +38,10 @@ def call() {
                     node('nested-agent') {
                         container('busybox') {
                             stage('Run Hello World in Nested Pod') {
-                                sh '''
-                                    echo "Reading IP address of busybox-agent pod from file"
-                                    BUSYBOX_AGENT_IP=$(cat /tmp/busybox-agent-ip.txt)
-                                    echo "busybox-agent Pod IP: $BUSYBOX_AGENT_IP"
-                                '''
+                                script {
+                                    echo "Using busybox-agent Pod IP: ${env.POD_IP}"
+                                    sh "echo busybox-agent Pod IP: ${env.POD_IP}"
+                                }
                             }
                             stage('Create Hello World File in Nested Pod') {
                                 sh 'echo "Hello, World!" > /tmp/hello.txt'

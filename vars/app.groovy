56eba1dd8ccba9fc178d092672e849fe728d81e6
Artur Zheludkov <rurkss@gmail.com>
2024-06-18 11:13:57 -0400

    Update app.groovy

--- vars/app.groovy
+++ vars/app.groovy
@@ -10,7 +10,9 @@ def call() {
                 ttyEnabled: true,
                 resourceRequestMemory: getResources().requests.memory,
                 resourceRequestCpu: getResources().requests.cpu,
-                resourceLimitMemory: getResources().limits.memory
+                resourceLimitMemory: getResources().limits.memory,
+                resourceRequestEphemeralStorage: '1Gi',
+                resourceLimitEphemeralStorage: '2Gi'
             )
         ]
     ) {
@@ -49,51 +51,5 @@ def call() {
         }
     }
 
-    int maxParallelTasks = 3
-    int totalTasks = env.LOOP_COUNT.toInteger()
-    int stagesRequired = (totalTasks + maxParallelTasks - 1) / maxParallelTasks // Ceiling division
-
-    for (int stageIndex = 0; stageIndex < stagesRequired; stageIndex++) {
-        stage("Parallel Stage ${stageIndex + 1}") {
-            def parallelStages = [:]
-            for (int i = 0; i < maxParallelTasks; i++) {
-                int taskIndex = stageIndex * maxParallelTasks + i
-                if (taskIndex < totalTasks) {
-                    def index = taskIndex // need to capture the loop variable
-                    parallelStages["Nested Pod Task ${index + 1}"] = {
-                        podTemplate(
-                            cloud: getCloud(),
-                            label: "nested-agent-${index}",
-                            containers: [
-                                multyContainerTemplate(index)
-                            ]
-                        ) {
-                            node("nested-agent-${index}") {
-                                container('busybox') {
-                                    stage('Unstash Artifacts in Nested Pod') {
-                                        script {
-                                            echo "Files before unstashing:"
-                                            sh 'ls -l'
-                                            unstash 'artifacts-tar-gz'
-                                            echo "Files after unstashing:"
-                                            sh 'ls -l'
-                                        }
-                                    }
-                                    stage('Extract Artifacts in Nested Pod') {
-                                        sh '''
-                                            tar -xf artifacts.tar.gz
-                                            echo "Current working directory after decompressing the archive:"
-                                            pwd
-                                            ls -l
-                                        '''
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            parallel parallelStages
-        }
-    }
+    parallelStagesLoop(env.LOOP_COUNT.toInteger(), 3)
 }

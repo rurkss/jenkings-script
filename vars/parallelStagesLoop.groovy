4fbf45d651a0fddb26aca1b6ef898c95ade997a8
Artur Zheludkov <rurkss@gmail.com>
2024-06-20 15:37:36 -0400

    Update parallelStagesLoop.groovy

--- vars/parallelStagesLoop.groovy
+++ vars/parallelStagesLoop.groovy
@@ -19,6 +19,8 @@ def call(def testComponentNames, String webImage, int maxParallelTasks) {
                                 container('web') {
                                     stage("Download File for ${componentName}") {
                                         sh '''
+                                            pwd
+                                            ls -l
                                             wget -O artifacts.tar.gz  "https://dl.dropboxusercontent.com/scl/fi/7i3c35qq8881ikdm75qzw/jsdeps.tar.gz?rlkey=xfg8jtssr64puoecbi0itdzyh&st=ecnugmcl" --no-check-certificate
                                             echo "Files after downloading:"
                                             ls -l

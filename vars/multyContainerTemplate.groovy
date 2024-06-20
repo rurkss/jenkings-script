9a67aca3859c2545c46538df61c9a3840b4db23e
Artur Zheludkov <rurkss@gmail.com>
2024-06-20 15:41:14 -0400

    Update multyContainerTemplate.groovy

--- vars/multyContainerTemplate.groovy
+++ vars/multyContainerTemplate.groovy
@@ -27,6 +27,7 @@ def call(String webImage) {
                 envVar(key: 'NODE_BIN', value: '/home/app/.nvm/versions/node/v20.14.0/bin'),
                 envVar(key: 'HOME', value: '/home/app')
             ],
+            workingDir: '/home/app/src',
             resourceRequestMemory: '5Gi',
             resourceRequestCpu: '2000m',
             resourceRequestEphemeralStorage: '5Gi',

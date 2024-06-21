38210c20de16981ef9b94f0d8ab709cbda3588fd
Artur Zheludkov <rurkss@gmail.com>
2024-06-21 13:03:15 -0400

    update copy.sh

--- resources/scripts/extract_artifacts.sh
+++ resources/scripts/extract_artifacts.sh
@@ -1,3 +1,34 @@
-#!/bin/sh
-echo "Starting decompression with tar -xf..."
-tar -xf artifacts.tar.gz
+#!/bin/bash
+
+if [ -f "jsdeps.tar.gz" ]; then
+    echo "Starting decompression with tar -xf..."
+    time tar -xf jsdeps.tar.gz
+
+    if [ -d "node_modules" ]; then
+        echo "Moving node_modules to /home/app/src..."
+        mv node_modules /home/app/src/
+        echo "Move complete."
+    else
+        echo "node_modules folder not found."
+    fi
+
+    if [ -d "components" ]; then
+        for dir in components/*; do
+            if [ -d "$dir" ]; then
+                folder_name=$(basename "$dir")
+                if [ -d "$dir/node_modules" ]; then
+                    echo "Moving node_modules to /home/app/src/components/$folder_name..."
+                    mv "$dir/node_modules" "/home/app/src/components/$folder_name/"
+                    echo "Move complete for $folder_name."
+                else
+                    echo "node_modules not found in $folder_name."
+                fi
+            fi
+        done
+    else
+        echo "components directory not found."
+    fi
+    rm -rf jsdeps.tar.gz    
+else
+    echo "jsdeps.tar.gz not found."
+fi
--- vars/parallelStagesLoop.groovy
+++ vars/parallelStagesLoop.groovy
@@ -18,7 +18,7 @@ def call(def testComponentNames, String webImage, int maxParallelTasks) {
                             container('web') {
                                 stage("Download File for ${componentName}") {
                                     sh '''        
-                                        wget -O artifacts.tar.gz  "https://dl.dropboxusercontent.com/scl/fi/7i3c35qq8881ikdm75qzw/jsdeps.tar.gz?rlkey=xfg8jtssr64puoecbi0itdzyh&st=ecnugmcl" --no-check-certificate                                    
+                                        wget -O jsdeps.tar.gz  "https://dl.dropboxusercontent.com/scl/fi/7i3c35qq8881ikdm75qzw/jsdeps.tar.gz?rlkey=xfg8jtssr64puoecbi0itdzyh&st=ecnugmcl" --no-check-certificate                                    
                                         echo "Files after downloading:"
                                         ls -l
                                         pwd

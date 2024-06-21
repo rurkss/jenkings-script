321df9191a7ad6f87206fa2a6dd92d4803c1e561
Artur Zheludkov <rurkss@gmail.com>
2024-06-21 12:56:27 -0400

    Update parallelStagesLoop.groovy

--- vars/parallelStagesLoop.groovy
+++ vars/parallelStagesLoop.groovy
@@ -1,59 +1,60 @@
 def call(def testComponentNames, String webImage, int maxParallelTasks) {
     int totalTasks = testComponentNames.size()
-    int stagesRequired = (totalTasks + maxParallelTasks - 1)
+    def semaphore = new java.util.concurrent.Semaphore(maxParallelTasks)
 
-    for (int stageIndex = 0; stageIndex < stagesRequired; stageIndex++) {
-        stage("Parallel Stage ${stageIndex + 1}") {
-            def parallelStages = [:]
-            for (int i = 0; i < maxParallelTasks; i++) {
-                int taskIndex = stageIndex * maxParallelTasks + i
-                if (taskIndex < totalTasks) {
-                    def componentName = testComponentNames[taskIndex] 
-                    parallelStages["Test ${componentName}"] = {
-                        podTemplate(
-                            cloud: getCloud(),
-                            label: "nested-agent-${componentName}",
-                            containers: multyContainerTemplate(webImage)                                              
-                        ) {
-                            node("nested-agent-${componentName}") {
-                                container('web') {
-                                    stage("Download File for ${componentName}") {
-                                        sh '''                                            
-                                            echo "Files after downloading:"
-                                            ls -l
+    stage("Parallel Tasks") {
+        def parallelStages = [:]
+        for (int taskIndex = 0; taskIndex < totalTasks; taskIndex++) {
+            def componentName = testComponentNames[taskIndex]
+            parallelStages["Test ${componentName}"] = {
+                semaphore.acquire()
+                try {
+                    podTemplate(
+                        cloud: getCloud(),
+                        label: "nested-agent-${componentName}",
+                        containers: multyContainerTemplate(webImage)                                              
+                    ) {
+                        node("nested-agent-${componentName}") {
+                            container('web') {
+                                stage("Download File for ${componentName}") {
+                                    sh '''        
+                                        wget -O artifacts.tar.gz  "https://dl.dropboxusercontent.com/scl/fi/7i3c35qq8881ikdm75qzw/jsdeps.tar.gz?rlkey=xfg8jtssr64puoecbi0itdzyh&st=ecnugmcl" --no-check-certificate                                    
+                                        echo "Files after downloading:"
+                                        ls -l
+                                        pwd
+                                    '''
+                                }
+                                stage("Unstash Artifacts for ${componentName}") {
+                                    script {
+                                        echo "Files before unstashing:"
+                                        sh 'ls -l'
+                                        unstash 'sample-txt'
+                                        // unstash 'artifacts-tar-gz'
+                                        echo "Files after unstashing:"
+                                        sh 'ls -l'
+                                    }
+                                }
+                                stage("Extract Artifacts for ${componentName}") {
+                                    script {
+                                        def scriptContent = libraryResource 'scripts/extract_artifacts.sh'
+                                        writeFile file: 'extract_artifacts.sh', text: scriptContent
+                                        sh '''
+                                            ls -all
+                                            sh extract_artifacts.sh                                                
+                                            echo "Current working directory after running the script:"
                                             pwd
+                                            ls -l
                                         '''
                                     }
-                                    // stage("Unstash Artifacts for ${componentName}") {
-                                    //     script {
-                                    //         echo "Files before unstashing:"
-                                    //         sh 'ls -l'
-                                    //         unstash 'sample-txt'
-                                    //         // unstash 'artifacts-tar-gz'
-                                    //         echo "Files after unstashing:"
-                                    //         sh 'ls -l'
-                                    //     }
-                                    // }
-                                    // stage("Extract Artifacts for ${componentName}") {
-                                    //     script {
-                                    //         def scriptContent = libraryResource 'scripts/extract_artifacts.sh'
-                                    //         writeFile file: 'extract_artifacts.sh', text: scriptContent
-                                    //         sh '''
-                                    //             ls -all
-                                    //             sh extract_artifacts.sh                                                
-                                    //             echo "Current working directory after running the script:"
-                                    //             pwd
-                                    //             ls -l
-                                    //         '''
-                                    //     }
-                                    // }
                                 }
                             }
                         }
                     }
+                } finally {
+                    semaphore.release()
                 }
             }
-            parallel parallelStages
         }
+        parallel parallelStages
     }
 }

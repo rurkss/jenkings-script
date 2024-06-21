73c64f394678d80e850f92173d1208f976630e93
Artur Zheludkov <rurkss@gmail.com>
2024-06-21 11:48:20 -0400

    Update parallelStagesLoop.groovy

--- vars/parallelStagesLoop.groovy
+++ vars/parallelStagesLoop.groovy
@@ -13,7 +13,8 @@ def call(def testComponentNames, String webImage, int maxParallelTasks) {
                         podTemplate(
                             cloud: getCloud(),
                             label: "nested-agent-${componentName}",
-                            containers: multyContainerTemplate(webImage)                     
+                            containers: multyContainerTemplate(webImage),
+                            workingDir: '/jenkins',                     
                         ) {
                             node("nested-agent-${componentName}") {
                                 container('web') {

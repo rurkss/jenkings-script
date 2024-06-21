fcd9a49d7996fb0db600a2404877abcf17fc1a48
Artur Zheludkov <rurkss@gmail.com>
2024-06-21 12:34:59 -0400

    Update multyContainerTemplate.groovy

--- vars/multyContainerTemplate.groovy
+++ vars/multyContainerTemplate.groovy
@@ -28,7 +28,7 @@ def call(String webImage) {
                 envVar(key: 'NODE_BIN', value: '/home/app/.nvm/versions/node/v20.14.0/bin'),
                 envVar(key: 'HOME', value: '/home/app')
             ],
-            workingDir: '/jenkins-agent',
+            // workingDir: '/jenkins-agent',
             resourceRequestMemory: '5Gi',
             resourceRequestCpu: '2000m',
             resourceRequestEphemeralStorage: '3Gi',

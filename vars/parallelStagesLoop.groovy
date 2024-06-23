83eae8d3d4c6ca376c8262b9d026da8c4632dfda
Artur Zheludkov <rurkss@gmail.com>
2024-06-23 09:24:19 -0400

    Update parallelStagesLoop.groovy

--- vars/parallelStagesLoop.groovy
+++ vars/parallelStagesLoop.groovy
@@ -74,7 +74,7 @@ def call(def testComponentNames, String webImage, int maxParallelTasks) {
                                 stage("Run Test ${componentName}") {
                                     script {
                                         def output = sh (
-                                            script: '''bash -lc 'CI=true RAILS_ENV=test DATABASE_NAME=accounting cd /home/app/src && bin/cobra cmd accounting ci --no-interactive' ''',
+                                            script: '''CI=true RAILS_ENV=test DATABASE_NAME=accounting cd /home/app/src && bin/cobra cmd accounting ci --no-interactive ''',
                                             returnStdout: true,
                                         )
                                         echo "Command output: ${output}"

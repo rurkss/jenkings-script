b78aa7efcea79814158f98eda2bb2113f10b85df
Artur Zheludkov <rurkss@gmail.com>
2024-06-23 11:58:43 -0400

    Update run_test.sh

--- resources/scripts/run_test.sh
+++ resources/scripts/run_test.sh
@@ -5,7 +5,8 @@ export CI=true
 export RAILS_ENV=test
 export DATABASE_NAME=accounting
 
+id
 pwd
 ls -all
 # Run the command and output to stdout
-bash bin/cobra cmd accounting ci --no-interactive
+bash -lc 'bin/cobra cmd accounting ci --no-interactive'

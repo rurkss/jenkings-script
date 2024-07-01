52a5cf842663e177114e463f0504af12e2b0ada5
Artur Zheludkov <rurkss@gmail.com>
2024-07-01 12:24:19 -0400

    Update app.groovy

--- vars/app.groovy
+++ vars/app.groovy
@@ -1,5 +1,5 @@
 def call() {
-    def testComponentNames = ["sales", "sales", "sales"]
+    def testComponentNames = ["sales"]
     String webImage = 'image-registry.powerapp.cloud/nitro-web/nitro_web:PR-40151-1b6d0a2402609c9a11f94c925e8f077e1b8fd47f-62'
     int maxParallelTasks = 3
     String yamlFilePath = 'multicontainer.yaml'

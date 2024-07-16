7c4746ac5266d90ce82a2e32a62aae74c3ef3658
Artur Zheludkov <rurkss@gmail.com>
2024-07-16 14:48:58 -0400

    Update parallelStagesLoop.groovy

--- vars/parallelStagesLoop.groovy
+++ vars/parallelStagesLoop.groovy
@@ -11,6 +11,7 @@ def call(def testComponentNames, String webImage, int maxParallelTasks) {
                 semaphore.acquire() 
                 try {
                     podTemplate(
+                        inheritFrom: '',
                         cloud: getCloud(),
                         label: label,                        
                         containers: multyContainerTemplate(webImage),

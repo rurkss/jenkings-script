e2f0c2d9f919f7ade4c55219fedf1c3234fb7525
Artur Zheludkov <rurkss@gmail.com>
2024-07-17 13:31:55 -0400

    Update app.groovy

--- vars/app.groovy
+++ vars/app.groovy
@@ -4,51 +4,51 @@ def call() {
     int maxParallelTasks = 3
     String yamlFilePath = 'multicontainer.yaml'
 
-    podTemplate(cloud: getCloud(), name: 'bparent', volumes: [
+    podTemplate(
+        cloud: getCloud(),
+        label: 'busybox-agent',
+        containers: [
+            containerTemplate(
+                name: 'web',
+                image: 'busybox:latest',
+                command: 'cat',
+                ttyEnabled: true,
+                resourceRequestMemory: getResources().requests.memory,
+                resourceRequestCpu: getResources().requests.cpu,
+                resourceLimitMemory: getResources().limits.memory,
+                resourceRequestEphemeralStorage: '512Mi',
+                resourceLimitEphemeralStorage: '512Mi',                
+            )
+        ],
+        imagePullSecrets: ['image-registry-prod-robot-powerhome'],
+        volumes: [
             dynamicPVC(mountPath: '/home/config', requestsSize: '5Gi', storageClassName: 'file', accessModes: 'ReadWriteMany'),
-        ], containers: [containerTemplate(image: 'busybox', name: 'bparent', command: 'cat', ttyEnabled: true)]) {
-        podTemplate(
-            cloud: getCloud(),
-            label: 'busybox-agent',
-            containers: [
-                containerTemplate(
-                    name: 'busybox',
-                    image: 'busybox:latest',
-                    command: 'cat',
-                    ttyEnabled: true,
-                    resourceRequestMemory: getResources().requests.memory,
-                    resourceRequestCpu: getResources().requests.cpu,
-                    resourceLimitMemory: getResources().limits.memory,
-                    resourceRequestEphemeralStorage: '512Mi',
-                    resourceLimitEphemeralStorage: '512Mi'                
-                )
-            ]
-        ) {
-            node('busybox-agent') {
-                def downloadAndStash = { String url, String outputFile, String stashName ->
-                    container('busybox') {
-                        sh "wget -O /home/config/${outputFile} \"${url}\" --no-check-certificate"
-                        // stash includes: "${outputFile}", name: "${stashName}"
+        ]
+    ) {
+        node('busybox-agent') {
+            def downloadAndStash = { String url, String outputFile, String stashName ->
+                container('busybox') {
+                    sh "wget -O /home/config/${outputFile} \"${url}\" --no-check-certificate"
+                    // stash includes: "${outputFile}", name: "${stashName}"
+                }
+            }
+
+            stage('Download and Stash YAMLs') {
+                parallel (
+                    'Config.yml': {
+                        downloadAndStash("https://dl.dropboxusercontent.com/scl/fi/svdpth34f16dfxuohb4ej/config.yml?rlkey=va8hqpr82wp2c5i5g56tc9vmx&st=pr7mhaoy", 'config.yml', 'config-yml')
+                    },
+                    'Ldap.yml': {
+                        downloadAndStash("https://dl.dropboxusercontent.com/scl/fi/v1pe0vri61j0mj0lctquu/ldap.yml?rlkey=nak1v3kqxoam6h4f3kjkbye7g&st=vnbvlqrz", 'ldap.yml', 'ldap-yml')                        
+                    },
+                    'Database.yml': {
+                        downloadAndStash("https://dl.dropboxusercontent.com/scl/fi/7368hynz2bz75cl13zqlm/database.yml?rlkey=1z4jm6qjvy95jrb11nmhxczlo&st=ry5b6cbq", 'database.yml', 'database-yml')                                                                        
                     }
-                }
+                )
+            }
 
-                stage('Download and Stash YAMLs') {
-                    parallel (
-                        'Config.yml': {
-                            downloadAndStash("https://dl.dropboxusercontent.com/scl/fi/svdpth34f16dfxuohb4ej/config.yml?rlkey=va8hqpr82wp2c5i5g56tc9vmx&st=pr7mhaoy", 'config.yml', 'config-yml')
-                        },
-                        'Ldap.yml': {
-                            downloadAndStash("https://dl.dropboxusercontent.com/scl/fi/v1pe0vri61j0mj0lctquu/ldap.yml?rlkey=nak1v3kqxoam6h4f3kjkbye7g&st=vnbvlqrz", 'ldap.yml', 'ldap-yml')                        
-                        },
-                        'Database.yml': {
-                            downloadAndStash("https://dl.dropboxusercontent.com/scl/fi/7368hynz2bz75cl13zqlm/database.yml?rlkey=1z4jm6qjvy95jrb11nmhxczlo&st=ry5b6cbq", 'database.yml', 'database-yml')                                                                        
-                        }
-                    )
-                }
-
-                stage('Run Tests') {
-                    parallelStagesLoop(testComponentNames, webImage, maxParallelTasks)
-                }
+            stage('Run Tests') {
+                parallelStagesLoop(testComponentNames, webImage, maxParallelTasks)
             }
         }
     }
